f078731d59698d18e1d2259155c5b2ba6dc9825e
Erdem ISGUDER <erdemisg@gmail.com>
2019-10-05 23:14:28 +0300

    [DEV][redugsi][develop][ Item combination selected animation was created ]

--- TFT Asistant/View/SquareCollectionViewCell/SquareCollectionViewCell.swift
+++ TFT Asistant/View/SquareCollectionViewCell/SquareCollectionViewCell.swift
@@ -11,10 +11,27 @@ import UIKit
 class SquareCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var borderView: UIView!
+    
+    override var isSelected: Bool{
+        didSet{
+            UIView.animate(withDuration: 0.3, animations: {[weak self] in
+                if let self = self{
+                    self.imageView.alpha = self.isSelected ? 1.0 : 0.6
+                    self.borderView.alpha = self.isSelected ? 1.0 : 0.0
+                }
+            })
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        initView()
+    }
+    
+    private func initView(){
+        borderView.layer.borderColor = UIColor(named: "color_pink")?.cgColor
+        borderView.layer.borderWidth = 2.0
     }
 
 }
