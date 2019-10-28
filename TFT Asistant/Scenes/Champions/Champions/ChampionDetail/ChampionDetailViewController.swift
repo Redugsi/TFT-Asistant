ab319d783944d8fd03d34c499469e1e15caeeff5
Erdem ISGUDER <erdemisg@gmail.com>
2019-10-28 19:57:53 +0300

    Worked But scrollview just strecth

--- TFT Asistant/Scenes/Champions/Champions/ChampionDetail/ChampionDetailViewController.swift
+++ TFT Asistant/Scenes/Champions/Champions/ChampionDetail/ChampionDetailViewController.swift
@@ -19,68 +19,66 @@ protocol ChampionDetailDisplayLogic: class
 
 class ChampionDetailViewController: UIViewController, ChampionDetailDisplayLogic
 {
-  var interactor: ChampionDetailBusinessLogic?
-  var router: (NSObjectProtocol & ChampionDetailRoutingLogic & ChampionDetailDataPassing)?
-
-  // MARK: Object lifecycle
-  
-
-    @IBOutlet weak var scrollViewHeight: NSLayoutConstraint!
+    var interactor: ChampionDetailBusinessLogic?
+    var router: (NSObjectProtocol & ChampionDetailRoutingLogic & ChampionDetailDataPassing)?
+    
+    // MARK: Object lifecycle
+    
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var championDetailView: ChampionDetailView!
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup()
-  {
-    let viewController = self
-    let interactor = ChampionDetailInteractor()
-    let presenter = ChampionDetailPresenter()
-    let router = ChampionDetailRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
     }
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    interactor?.getChampionDetail()
-  }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup()
+    {
+        let viewController = self
+        let interactor = ChampionDetailInteractor()
+        let presenter = ChampionDetailPresenter()
+        let router = ChampionDetailRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: Routing
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        interactor?.getChampionDetail()
+    }
     
     func displayChampionDetail(championViewModel: Champions.ChampionViewModel) {
         championDetailView.viewModel = championViewModel
-        print(championDetailView.frame)
     }
-        
+    
     private func calculateHeight(){
         print(scrollView.contentSize)
         var contentRect = CGRect.zero
